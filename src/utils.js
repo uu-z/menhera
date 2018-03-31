42004bca3b571b8f35f645119f576ff5710a587e
cirnoo <ncysatnaf@gmail.com>
2018-03-31 19:19:55 +0800

    fix configMerger

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-export const ConfigMerger = (Obj1, Obj2) => {
+export const ConfigMerger = (Obj1 = {}, Obj2 = {}) => {
   let cache = Obj1;
   Object.keys(Obj2).forEach(key => {
     if (Array.isArray(Obj2[key])) {
       cache[key] = Array.from(new Set([...(Obj1[key] || []), ...Obj2[key]]));
-    } else if (typeof Obj2[key] === "obj") {
+    } else if (typeof Obj2[key] === "object") {
       cache[key] = ConfigMerger(Obj1[key], Obj2[key]);
     } else {
       cache[key] = Obj2[key];
