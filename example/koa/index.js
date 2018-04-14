3f225f2dcf31137b8480a0578761fbf485b38a04
cirnoo <ncysatnaf@gmail.com>
2018-04-14 15:01:29 +0800

    update example

--- example/koa/index.js
+++ example/koa/index.js
@@ -1,25 +1,18 @@
 import Menhera from "../../src";
 import * as routes from "./routers";
 import * as controllers from "./controllers";
-import { hooks } from "./menhera-koa";
-
-const app = {
-  name: "koa",
-  _hooks: {
-    koa: hooks
-  }
-};
+import { hooks as koa } from "./menhera-koa";
 
 const _ = new Menhera({
-  _mount: {
-    app
+  _hooks: {
+    koa
   },
   koa: {
     data: {
       test: { index: 0, user: "" }
     },
     controllers,
-    routes
-  },
-  listen: 3000
+    routes,
+    listen: 3000
+  }
 });
