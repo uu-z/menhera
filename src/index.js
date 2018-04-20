f6a28ddb7536bf3e9c2e28b64e123295df5b0434
nano <ncysatnaf@gmail.com>
2018-04-20 17:49:35 +0800

    delegate event

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import { initHooks, $get, $set, $use, _hooks, _mount } from "./utils";
+import { EventEmitter } from "events";
 
 export * from "./utils";
 
 export const $core = (_, _object) => {
   _.hooks = initHooks(_);
   _.hooks._hooks = { _: [_hooks] };
-  _.$use = _object => $use(_, _object);
-  _.$get = _object => $get(_, _object);
-  _.$set = _object => $set(_, _object);
+  _.events = new EventEmitter();
+  _.events.on("$use", _object => $use(_, _object));
+  _.events.on("$get", _object => $get(_, _object));
+  _.events.on("$set", _object => $set(_, _object));
+  _.$use = _object => _.events.emit("$use", _object);
+  _.$get = _object => _.events.emit("$get", _object);
+  _.$set = _object => _.events.emit("$set", _object);
+
   _.$use({ _hooks: { _mount } });
   _.$use(_object);
   return _;
