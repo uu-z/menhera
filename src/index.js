f3bd6c8ad33975f984f512792f8c973ae96a6500
cirnoo <ncysatnaf@gmail.com>
2018-05-03 22:34:35 +0800

    v0.7.2

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ import { EventEmitter } from "events";
 
 export * from "./utils";
 
-export const $minCore = (_, _object) => {
-  _[HOOKS] = { _: [_hooks._] };
-  _.$use = (_, _object) => $use(_, _object);
-  _.$use(_object);
-  return _;
-};
-
 export const $core = (_, _object) => {
   _[HOOKS] = initHooks(_);
   _.events = new EventEmitter();
@@ -29,12 +22,6 @@ export const $core = (_, _object) => {
     _.events.emit("$use", _object);
     return _;
   };
-  _.$get = _object => $get(_, _object);
-  _.$set = _object => $set(_, _object);
-  _.$diff = _object => $diff(_, _object);
-  _.$has = _object => $has(_, _object);
-  _.$match = _object => $match(_, _object);
-  _.$merge = _array => $merge(_array);
   _.$use(_object);
   return _;
 };
