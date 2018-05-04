c2ccb9a888d6ca7efa2a56aa4dd049cd4aaeb6cc
nano <ncysatnaf@gmail.com>
2018-05-04 21:26:30 +0800

    get property by universal path

--- example/GetSet.js
+++ example/GetSet.js
@@ -2,19 +2,13 @@ import Mhr, { $set, $get } from "../dist";
 
 let test = {};
 $set(test, {
-  foo: {
-    bar: 1
-  },
-  "foo.dot": 2,
-  "foo.slash": 3
+  "foo.dot": 123,
+  "foo.slash": 456
 });
 console.log(test);
 
 console.log(
   $get(test, {
-    foo: {
-      bar: ""
-    },
     slash: "foo/dot",
     dot: "foo.slash"
   })
